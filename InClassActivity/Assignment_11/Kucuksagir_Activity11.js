41768fe8577c6b1237b6cb8a1ef0ef2feaf0dd19
Özlem Küçüksağır <ozlemkucuksagir2@gmail.com>
2023-06-06 15:03:34 +0300

    Assignment 11 done.

---
+++ InClassActivity/Assignment_11/Kucuksagir_Activity11.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+    // Click event handler for sidebar links
+    $("#nav_list a").click(function (event) {
+        event.preventDefault(); 
+        var speakerFile = "json_files/" + $(this).attr("title") + ".json"; // name of JSON file
+        // Clear 
+        $("main").empty();
+        $.getJSON(speakerFile, function (data) {
+            debugger;           
+            data.speakers.forEach(function (speaker) {                
+                var speakerHtml =
+                    '<h1>' + speaker.title + '</h1>' +
+                    '<img src="' + speaker.image + '">' +
+                    '<h2>' + speaker.month + '<br>' + speaker.speaker + '</h2>' +
+                    '<p>' + speaker.text + '</p>';
+                // Append HTML elements to main section
+                $("main").append(speakerHtml);
+            });
+
+        });
+    });
+});
